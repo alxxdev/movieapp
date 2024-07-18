a7d422b40b0e15fae23e35aed90788780b7af053
alxxdev <alxxdev@yandex.ru>
2024-07-18 21:20:44 +0300

    commit

--- rating/internal/repository/memory/memory.go
+++ rating/internal/repository/memory/memory.go
@@ -18,13 +18,21 @@ func New() *Repository {
 }
 
 // Get retrieves all ratings for a given record.
-func (r *Repository) Get(ctx context.Context, recordID model.RecordID, recodrdType model.RecordType) ([]model.Rating, error) {
+func (r *Repository) Get(ctx context.Context, recordID model.RecordID, recordType model.RecordType) ([]model.Rating, error) {
 	if _, ok := r.data[recordType]; !ok {
 		return nil, repository.ErrNotFound
 	}
-	if ratings, ok := r.data[recordType][recordID]; !ok ||
-		len(ratings) == 0 {
+	if ratings, ok := r.data[recordType][recordID]; !ok || len(ratings) == 0 {
 		return nil, repository.ErrNotFound
 	}
 	return r.data[recordType][recordID], nil
+}
+
+// Put adds a rating for a given record.
+func (r *Repository) Put(ctx context.Context, recordID model.RecordID, recordType model.RecordType, rating *model.Rating) error {
+	if _, ok := r.data[recordType]; !ok {
+		r.data[recordType] = map[model.RecordID][]model.Rating{}
+	}
+	r.data[recordType][recordID] = append(r.data[recordType][recordID], *rating)
+	return nil
 }
